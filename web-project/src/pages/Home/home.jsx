66a186619e25967ec8fb2f525de0ef3936a34661
Amel Plojovic <amelplojovic67@gmail.com>
2024-09-12 17:33:12 +0200

    test

--- web-project/src/pages/Home/home.jsx
+++ web-project/src/pages/Home/home.jsx
@@ -1,6 +1,11 @@
-function Home (){
-    return(
-        <></>
-    )
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
-export default Home
+
+export default Home;
